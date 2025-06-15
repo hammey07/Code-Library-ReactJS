a71e55389bb27a97c69f83d41c1c71a5a18279e0
hammey07 <hammey07.hs@gmail.com>
2025-06-15 20:09:13 +0100

    data file updated

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -7,17 +7,17 @@ export default function usePosts() {
 
   useEffect(() => {
     async function loadPosts() {
-      const response = await fetch(
-        "http://local.components-library.com/wp-json/wp/v2/posts"
-      );
-      if (!response.ok) {
-        console.log("Error fetching API data"); // oups! something went wrong
-        return;
-      }
+      // const response = await fetch(
+      //   "http://local.components-library.com/wp-json/wp/v2/posts"
+      // );
+      // if (!response.ok) {
+      //   console.log("Error fetching API data"); // oups! something went wrong
+      //   return;
+      // }
 
-      const posts = await response.json();
-      setPosts(posts);
-      // setPosts(data);
+      // const posts = await response.json();
+      // setPosts(posts);
+      setPosts(data);
       setPostLoading(false);
     }
 
